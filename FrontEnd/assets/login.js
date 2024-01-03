b986842437b4a5a45ca76db364c635c82e4da808
Thibault G <thibault.gerardin@gmail.com>
2024-01-03 17:15:46 +0100

    avancée sur le login, création de la modale, modifications du CSS, modifications fonction createfigure pour intégrer le foncitonnement différent avec la modale

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,1 +1,47 @@
 // Page js pour la fonction post qui va permettre de se login
+
+
+function login(email, mdp) {
+    fetch('http://localhost:5678/api/users/login', {
+        method : "POST",
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: mdp,
+          }),
+        }
+    )
+    .then((response) => response.json())
+    // rajouter le localstorage pour "sauvegarder" le fait d'etre logged in
+    .then((data) => {
+        let tokenValue = JSON.stringify(data['token']) 
+        if (data.error) {
+          alert("Utilisateur ou mot de passe non reconnu")
+        } else {
+            // reste à trouver comment selectionner sur index.html
+
+        localStorage.setItem('token', tokenValue); 
+        location.href = "index.html"      
+
+        }
+      })
+    .catch((err) => {
+      console.log(err);
+     });
+    
+}
+
+
+
+
+const submit = document.getElementById("btn-submit")
+
+submit.addEventListener("click", (event) => {
+    event.preventDefault()
+    const email = document.getElementById("login-email").value
+    const mdp = document.getElementById("mdp").value
+    login(email, mdp)
+})
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,5 +1,27 @@
 // Script à écrire pour fetch les works sur l'API (http://localhost:5678/api/works) et créer l'HTML pour afficher la page d'accueil dynamiquement
 
+// Cherche si le token de login est présent et passe en mode édition si c'est OK
+let token = window.localStorage.getItem('token')
+
+if (token !== null) {
+     document.querySelector('.edition').style.display = 'flex'
+     document.querySelector('.filtres').style.display = 'none'
+     document.getElementById('logout-link').style.display = 'flex'
+     document.getElementById('login-link').style.display = 'none'
+     document.getElementById('modif').style.display = 'flex'
+
+}
+
+// comportement du lien logout 
+
+const logout = document.getElementById('logout-link')
+logout.addEventListener('click', function () {
+    window.localStorage.clear()
+    // recharge la page après le "reset" mais ne fonctionne pas pour le moment ?
+    window.location.reload()
+})
+
+
 
 
 // let works = window.localStorage.getItem('works')
@@ -25,12 +47,13 @@ let works = window.localStorage.getItem('works')
 works = JSON.parse(works)
 
 
-// fonction qui créer les vignettes sur la page d'accueil en fonction des données récupérées sur l'API
-function createFigure(works) {
-    // clear the gallery
-    const gallery = document.querySelector(".gallery")
-    gallery.innerHTML=""
+// fonction qui créer les vignettes sur un "lieu" donné (page d'accueil => gallery ou modale => mini-galery )
+function createFigure(works, lieu) {
+    // clear l'endroit où vont apparaitre les images
+    const location = document.querySelector(`.${lieu}`)
+    location.innerHTML=""
     
+   
     for (let i=0; i < works.length; i++) {
 
         const vignette = works[i]
@@ -38,17 +61,31 @@ function createFigure(works) {
         const imgVignette = document.createElement("img")
         imgVignette.src = vignette.imageUrl
         imgVignette.alt = vignette.title
-        const captionVignette = document.createElement("figcaption")
-        captionVignette.innerText = vignette.title
+       
         const figure = document.createElement("figure")
-        gallery.append(figure)
-        figure.append(imgVignette)
-        figure.append(captionVignette)
+        console.log(location)
+        // comportement si fonction lancée dans la modale pour ajout bouton supprimer et retrait du caption
+        if (location.className === `mini-gallery`) {
+            const trash = document.createElement("button")
+            trash.className = 'btn-trash'
+            trash.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+            location.append(figure)
+            figure.append(imgVignette)    
+            figure.append(trash)
+        } else {
+            const captionVignette = document.createElement("figcaption")
+            captionVignette.innerText = vignette.title  
+            location.append(figure)
+            figure.append(imgVignette)      
+            figure.append(captionVignette)        
+        }
+          
+      
    
 }
 }
 
-createFigure(works)
+createFigure(works, 'gallery')
 
 
 // Utilisation du filtre Objets pour retourner les travaux avec la catégorie Objets uniquement
@@ -56,35 +93,46 @@ const boutonObjets = document.getElementById("objets");
 
 boutonObjets.addEventListener("click", function () {
     const worksObjet = works.filter(function (works) {
-        return works.category.id === 1;
-    });
-    createFigure(worksObjet);
-});
+        return works.category.id === 1
+    })
+    createFigure(worksObjet, 'gallery')
+})
 
 // Utilisation du filtre Appartements pour retourner les travaux avec la catégorie Appartements uniquement
 const boutonAppartements = document.getElementById("appartements");
 
 boutonAppartements.addEventListener("click", function () {
     const worksApparttements = works.filter(function (works) {
-        return works.category.id === 2;
-    });
-    createFigure(worksApparttements);
-});
+        return works.category.id === 2
+    })
+    createFigure(worksApparttements, 'gallery')
+})
 
 // Utilisation du filtre Hotels et restaurants pour retourner les travaux avec la catégorie Hotels et restaurants uniquement
 const boutonHotelsetresto = document.getElementById("hotelsetresto");
 
 boutonHotelsetresto.addEventListener("click", function () {
     const worksHotelsetresto = works.filter(function (works) {
-        return works.category.id === 3;
-    });
-    createFigure(worksHotelsetresto);
-});
+        return works.category.id === 3
+    })
+    createFigure(worksHotelsetresto, 'gallery')
+})
 
 // Filtre tous pour reset
 
 const boutonTous = document.getElementById("tous");
 
 boutonTous.addEventListener("click", function () {
-    createFigure(works);
-});
+    createFigure(works, 'gallery')
+})
+
+// ouverture de la modale
+
+const dialog = document.querySelector("dialog")
+const modif = document.getElementById("modif")
+
+modif.addEventListener("click", (event) => {
+    event.preventDefault()
+    dialog.showModal()
+    createFigure(works, 'mini-gallery')
+  })
